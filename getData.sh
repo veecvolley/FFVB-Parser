c859ee1cd58862a1d63f008ef8b0c29bb0e5e1af
Alexandre VOINIER <alexandre.voinier@afleya.com>
2025-05-20 21:46:58 +0200

    [+] Add Script for getting data and first draft of python

---
+++ getData.sh
@@ -0,0 +1,2 @@
+curl -X POST 'https://www.ffvbbeach.org/ffvbapp/resu/vbspo_calendrier_export_club.php'   -H 'Content-Type: application/x-www-form-urlencoded'   -d 'cnclub=0775819&cal_saison=2024/2025&typ_edition=E&type=RES' > export20242025.csv
+iconv -f latin1 -t utf-8 export20242025.csv > export20242025_utf8.csv
--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-import requests
-from bs4 import BeautifulSoup
+import csv
 
-URL = "https://www.ffvbbeach.org/ffvbapp/resu/planning_club.php?aff_semaine=PRE&date_jour=1742598000&cnclub=0775819&x=1&y=9"
-page = requests.get(URL)
+gymnases = set()
+with open('export20242025_utf8.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=";", quotechar='"')
+    i = 0
+    for row in reader:
+        if i == 0:
+            i += 1
+            continue
+        if (row[12] != ''):
+            gymnases.add(row[12])
 
-print(page.text)
+print(gymnases)
+from PIL import Image, ImageFont, ImageDraw
 
-soup = BeautifulSoup(page.content, "html.parser")
+im = Image.open("veec_fond.png")
+
+print(im.format, im.size, im.mode)
+
+# create an image
+out = Image.new("RGB", (1000, 1200), (255, 255, 255))
+
+# get a font
+fnt = ImageFont.truetype("Geneva.ttf", 20)
+# get a drawing context
+d = ImageDraw.Draw(out)
+
+# draw multiline text
+text = ""
+for gymnase in gymnases:
+    text += f"{gymnase}\n"
 
 
+d.multiline_text((10, 10), text, font=fnt, fill=(0, 0, 0))
+
+im.paste(out, (1000, 1000))
+im.show()
